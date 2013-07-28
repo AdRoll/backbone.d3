9c2e44160d3117318ceb0302fb3ba6b1470191c4
Jyri Tuulos <jyri.tuulos@gmail.com>
2013-07-27 21:58:17 -0700

    add options for chart width and height, fall back to element dimensions

--- js/mackerel.js
+++ js/mackerel.js
@@ -63,12 +63,15 @@ var Chart = Mackerel.Chart = Backbone.View.extend({
 
         // D3 margin convention
         // http://bl.ocks.org/mbostock/3019563
-        var margin = this.options.margin;
-        this.width = this.$el.width() - margin.left - margin.right;
-        this.height = this.$el.height() - margin.top - margin.bottom;
+        var margin = this.options.margin,
+            width = this.options.width || this.$el.width(),
+            height = this.options.height || this.$el.height();
+
+        this.width = width - margin.left - margin.right;
+        this.height = height - margin.top - margin.bottom;
         this.svg = d3.select(this.el).append('svg')
-            .attr('width', this.width + margin.left + margin.right)
-            .attr('height', this.height + margin.top + margin.bottom)
+            .attr('width', width)
+            .attr('height', height)
             .append('g')
                 .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
