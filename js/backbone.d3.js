ccb875dfb983809e4d40e7b855e66183255172a0
Jyri Tuulos <jyri.tuulos@gmail.com>
2013-10-13 21:21:02 -0700

    fix negative number handling in D3.format.shortNumber

--- js/backbone.d3.js
+++ js/backbone.d3.js
@@ -29,6 +29,8 @@ D3.VERSION = '0.1.0';
 
 D3.format = {
 
+    _siFormat: d3.format("s"),
+
     // Round number to N significant figures and show with SI prefix
     // e.g. shortNumber(12345) -> "12.3k"
     shortNumber: function(num, options) {
@@ -41,9 +43,11 @@ D3.format = {
         }, options);
 
         // http://blog.magnetiq.com/post/497605344/rounding-to-a-certain-significant-figures-in-javascript
-        var mult = Math.pow(10, opts.sigFigs - Math.floor(Math.log(num) / Math.LN10) - 1),
-            rounded = Math.round(num * mult) / mult;
-        return d3.format("s")(rounded);
+        var isNegative = num < 0,
+            abs = Math.abs(num),
+            mult = Math.pow(10, opts.sigFigs - Math.floor(Math.log(abs) / Math.LN10) - 1),
+            rounded = Math.round(abs * mult) / mult;
+        return (isNegative ? "-" : "") + D3.format._siFormat(rounded);
     },
 
     // Show full precision with thousands separators
